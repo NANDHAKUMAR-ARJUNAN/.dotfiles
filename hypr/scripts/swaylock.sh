b4aaec84dab4ab3b74d6809440abdc299e7c8120
NANDHAKUMAR-ARJUNAN <nandhakumarnkdr2006@gmail.com>
2025-09-09 21:26:14 +0530

    final config - install script v2

--- hypr/scripts/swaylock.sh
+++ hypr/scripts/swaylock.sh
@@ -1,2 +1,1 @@
-swayidle -w timeout 180 'swaylock -C /home/kabil/.config/sway_lock/config -f' timeout 240 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
- 
+swayidle -w timeout 180 'swaylock -C $HOME/.config/sway_lock/config -f' timeout 240 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
